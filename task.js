17023c12ce577418dd256145a822bd924249d8e5
David Mercier <david.mercier@cdlinc.ca>
2015-10-09 13:59:01 -0400

    testing template

--- task.js
+++ task.js
@@ -1,3 +1,21 @@
 /**
  * Created by dmercier on 2015-10-06.
  */
+
+Task = Backbone.Model.extend({
+
+    urlRoot: '/tasks',
+    default: {
+        UID: 0,
+        Description: 'My task'
+    },
+    initialize: function() {
+    }
+});
+
+var task = new Task();
+task.fetch({
+    success: function(task) {
+        alert(task.toJSON());
+    }
+})
--- todoapp.js
+++ todoapp.js
@@ -1,3 +1,39 @@
 /**
  * Created by dmercier on 2015-10-07.
  */
+
+var Tasks = Backbone.Collection.extend({
+    url: 'http://localhost:5000/tasks'
+});
+
+var TaskView = Backbone.View.extend({
+    el: '.page',
+    render: function() {
+        var that = this;
+        var tasks = new Tasks();
+
+        tasks.fetch( {
+            success: function(tasks) {
+                var template = _.template($('#task-list-template').html(), {tasks: tasks.models});
+
+                that.$el.html(template);
+            }
+        })
+    }
+});
+
+var Router = Backbone.Router.extend({
+    routes: {
+        '' : 'home' // intentionally blank for the home page
+    }
+});
+
+// Display logic
+var taskListView = new TaskView({ });
+var router = new Router();
+
+router.on('route:home', function() {
+    taskListView.render();
+});
+
+Backbone.history.start();
