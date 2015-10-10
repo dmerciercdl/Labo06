78efd891f2e952da50584e7220db41587fa5a660
David Mercier <david.mercier@cdlinc.ca>
2015-10-09 22:07:49 -0400

    les tâches marchent!!

--- todoapp.js
+++ todoapp.js
@@ -2,21 +2,31 @@
  * Created by dmercier on 2015-10-07.
  */
 
+var Task = Backbone.Model.extend({
+    'id': 0,
+    'task': 'toto'
+});
+
 var Tasks = Backbone.Collection.extend({
-    url: 'http://localhost:5000/tasks'
+    'url': 'http://localhost:5000/tasks',
+    'model': Task,
+
+    'parse': function( apiResponse ){
+        return apiResponse.tasks;
+    }
 });
 
 var TaskView = Backbone.View.extend({
-    el: '.page',
-    render: function() {
+    'el': '.page',
+    'template': _.template($('#task-list-template').html()),
+
+    'render': function() {
         var that = this;
         var tasks = new Tasks();
 
         tasks.fetch( {
-            success: function(tasks) {
-                var template = _.template($('#task-list-template').html(), {tasks: tasks.models});
-
-                that.$el.html(template);
+            success: function() {
+                that.$el.html( that.template( { 'tasks': tasks } ) );
             }
         })
     }
